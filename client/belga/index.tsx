719c639f24f40159091989028fca833e492087bd
Tomas Kikutis <t.kikutis@gmail.com>
2021-03-31 16:33:29 +0200

    Update package lock. Disable extensions for testing.

--- client/belga/index.tsx
+++ client/belga/index.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import angular from 'angular';
 import {IUser} from 'superdesk-api';
 import {startApp} from 'superdesk-core/scripts/index';
-import belgaImage from './image';
-import belga360Archive from './360archive';
-import belgaPress from './belgapress';
 import {AvatarContentText} from 'superdesk-ui-framework';
 
 class UserAvatar extends React.PureComponent<{user: Partial<IUser>}> {
@@ -26,8 +23,4 @@ setTimeout(() => {
     startApp([], {UserAvatar});
 });
 
-export default angular.module('belga', [
-    belgaImage.name,
-    belga360Archive.name,
-    belgaPress.name,
-]);
+export default angular.module('belga', []);
